162aadf29210b7b4dbc5bc8e40e2668007b380dc
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-04-16 23:15:00 +0300

    refactor: removal of excess

--- index.js
+++ index.js
@@ -3,14 +3,12 @@
 const puppeteer = require('puppeteer');
 
 async function helloWorld() {
-  // const browser = await puppeteer.launch({
-  //   executablePath: './node_modules/puppeteer/.local-chromium/win64-599821/chrome-win/chrome.exe' // TODO Get rid of it
-  // });
   const browser = await puppeteer.launch();
     
   const page = await browser.newPage();
 
   await page.goto('https://en.wikipedia.org/wiki/%22Hello,_World!%22_program');
+  
   await page.screenshot({ path: 'wikipedia-helloworld.png' });
 
   const firstPar = await page.$eval('#mw-content-text p', el => el.innerText);
